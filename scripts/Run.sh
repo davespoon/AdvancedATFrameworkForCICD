c1dbf1d73417e630b72500dece5080778820e7b8
Dmitriy Grinevich <Dmitriy.Grinevich@quorumsoftware.com>
2023-08-21 15:40:51 +0300

    Modify Run.sh and WaitAndRun.sh to generate living doc reports

--- scripts/Run.sh
+++ scripts/Run.sh
@@ -7,19 +7,23 @@ project="e2etest"
 
 cd "$(dirname "${0}")/.."
 
-
 export COMPOSE_HTTP_TIMEOUT=200
 
 docker-compose -p "$project" build
 
-docker-compose -p "$project" up -d ea_api ea_webapp db selenium-hub firefox chrome
+mkdir -m 777 "reports"
 
+docker-compose -p "$project" up -d ea_api ea_webapp db selenium-hub firefox chrome
 docker-compose -p "$project" up --no-deps ea_test
+
+docker cp ea_test: src/EETestBDD/LivingDoc.html ./reports
+echo "Specflow Living Doc report is copied to ./reports"
+ls -l ./reports
 
 exit_code=$(docker inspect ea_test -f '{{ .State.ExitCode }}')
 
 if [ $exit_code -eq 0 ]; then
-	exit $exit_code
+  exit $exit_code
 else
-	echo "Test Failed"
+  echo "Test Failed"
 fi
--- scripts/WaitAndRun.sh
+++ scripts/WaitAndRun.sh
@@ -4,13 +4,18 @@ set -e
 set -x
 
 until [ ]; do
-	dotnet "$1" && break
-	sleep 1
+  dotnet "$1" && break
+  sleep 1
 done
 
 until [ ]; do
-	sleep 30
-	curl -f "http://selenium-hub:4444/wd/hub/status" && break
+  sleep 30
+  curl -f "http://selenium-hub:4444/wd/hub/status" && break
 done
 
 dotnet test --logger "console;verbosity=detailed"
+
+dotnet tool install --global Specflow.Plus.LivingDoc.CLI
+export PATH="$PATH:/root/.dotnet/tools"
+echo "Starting Living Doc report generation"
+livingdoc test-assembly "src/EATestBDD/bin/Debug/net6.0/EATestBDD.dll" -t "src/EATestBDD/bin/Debug/net6.0/TestExecution.json"
